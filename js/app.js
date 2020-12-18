92432f2a0840e634a78a04ab78bafdb7617699b6
Sakaowduan <billmichelle33@gmail.com>
2020-12-18 20:01:44 +1100

    modify styles and app.js

--- js/app.js
+++ js/app.js
@@ -17,16 +17,25 @@
  * Define Global Variables
  * 
 */
-const navigation = document.getElementById('navbar__list');
-const sections = document.querySelectorAll('section');
 
+const sections = document.querySelectorAll("section");
+const navBar = document.querySelector("#navbar__list");
+const goToTopBtn = document.getElementById("goToTopBtn");
 /**
  * End Global Variables
  * Start Helper Functions
  * 
 */
 
-
+// Check element is in viewport 
+function isInViewport(element) {
+    const distance = element.getBoundingClientRect();
+    return (
+        distance.top <= 100 &&
+        distance.left >= 0 &&
+        distance.bottom >= 90 &&
+        distance.right <= (window.innerWidth || document.documentElement.clientWidth));
+}
 
 /**
  * End Helper Functions
@@ -34,36 +43,63 @@ const sections = document.querySelectorAll('section');
  * 
 */
 
-// build the nav
-const navBuilder = () => {
-    // store all navbars and then loop through all sections
-    let navUI = ``;
-    sections.forEach(section => {
-        const sectionID = section.id;
-        const sectionNavData = section.dataset.nav;
+// Build nav menubar
+function navbarBuilder () {
+    const fragment = document.createDocumentFragment();
+    for (const section of sections) {
+        let navItem = document.createElement('li');
+        const id = section.id;
+        const dataNav = section.dataset.nav;
+        navItem.innerHTML = `<a class="menu__link ${id}" href="#${id}">${dataNav}</a>`;
+        fragment.appendChild(navItem);
+    }
+    navBar.appendChild(fragment);
+}
 
-        navUI += `<li><a class="menu_link href="#${sectionID}">${sectionNavData}</a></li>`;
+navbarBuilder();
 
+// addEventlistener fires when a user scroll to any section
+// uses the isInviewport function to check and active link at the section 
+document.addEventListener('scroll', function activeSection(){
+    for (const section of sections) {
+        const navItem = document.querySelector(`.${section.id}`);
+        if (isInViewport(section)) {
+            section.classList.add("section-active");
+            navItem.classList.add("link-active");
+        } else {
+            section.classList.remove("section-active");
+            navItem.classList.remove("link-active");
+        }
+    }
+});
+
+// Smoothly scroll to the specific section after click on navigation bar
+const links = document.querySelectorAll(".menu__link");
+for (const link of links) {
+    link.addEventListener("click", function clickHandler(a){
+        a.preventDefault();
+        const href = document.querySelector(link.getAttribute("href"));
+        href.scrollIntoView({ behavior: "smooth" });
     });
-    // append all elements to the nevigation
-    navigation.innerHTML = navUI;
-};
+}
 
-navBuilder();
-// Add class 'active' to section when near top of viewport
+//Reveal "scroll to top" button after scrolling down the viewport
+window.onscroll = function() {scrollFunction()};
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        goToTopBtn.style.display = "block";
+      } else {
+        goToTopBtn.style.display = "none";
+      }
+}
 
-
-// Scroll to anchor ID using scrollTO event
-
-
+// addEventListener fires the scroll to top when click the the up icon
+goToTopBtn.addEventListener('click', goToTop());
+function goToTop() {
+    window.scrollTo({top: 0, behavior: 'smooth'});
+}
 /**
  * End Main Functions
  * Begin Events
  * 
 */
-
-// Build menu 
-
-// Scroll to section on link click
-
-// Set sections as active
